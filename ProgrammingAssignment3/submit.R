5991de44c932fed24a33c4be5d95a9e0e0f262b2
kravchukpetr <kravchukpetr@gmail.com>
2015-10-23 23:48:32 +0300

    R ProgrammingAssingment3

---
+++ ProgrammingAssignment3/submit.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/Пето/Documents/Coursera projects/R/ProgrammingAssignment3")
+source("best.R")
+source("submitscript3.R")
+submit()
---
+++ ProgrammingAssignment3/test.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/Пето/Documents/Coursera projects/R/ProgrammingAssignment3")
+source("best.R")
+best("SC", "heart attack")
+
+
+
+
+best("TX", "heart attack")
+##[1] "CYPRESS FAIRBANKS MEDICAL CENTER"
+best("TX", "heart failure")
+##[1] "FORT DUNCAN MEDICAL CENTER"
+best("MD", "heart attack")
+##[1] "JOHNS HOPKINS HOSPITAL, THE"
+best("MD", "pneumonia")
+##[1] "GREATER BALTIMORE MEDICAL CENTER"
